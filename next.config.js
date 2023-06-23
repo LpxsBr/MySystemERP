e6fbdbc0f205b51d9f9743680ea015bfdf43255f
LpxsBr <anselmolopes.an@gmail.com>
2023-06-23 13:01:11 -0300

    fix env

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
     compiler:{
         styledComponents: true,
-    },
-    env:{
-        NEXT_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
-        NEXT_GITHUB_ID: process.env.NEXT_PUBLIC_GITHUB_ID,
-        NEXT_GITHUB_SECRET: process.env.NEXT_PUBLIC_GITHUB_SECRET,
     }
 }
 
